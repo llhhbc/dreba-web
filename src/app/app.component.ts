f895c248689daada6a50b3434db8ee5df1d8e2ce
lilh <lilh>
2019-04-18 21:07:01 +0800

    add image upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 
-
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
 
 @Component({
   selector: 'app-root',
@@ -13,4 +11,23 @@ export class AppComponent {
   title = 'dreba';
 
   public Editor = ClassicEditor;
+
+  public model = {
+ editorData: '<p>Hello, worlddsaf!</p>'
+};
+
+  public config = {
+ckfinder: {
+  options: {
+    resourceType: 'Images'
+  },
+  uploadUrl: 'http://localhost:3000/test/upload/iamge'
+ }
+};
+
+public onChange( { editor } ) {
+const data = editor.getData();
+console.log( data );
+}
+
 }
