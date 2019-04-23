e6fc3dad81c0558684daee877988f10cdac14eae
lilh <lilh>
2019-04-23 23:07:51 +0800

    ckedit ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,58 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'dreba';
+	title = 'dreba';
 
-  public Editor = ClassicEditor;
+	constructor(private http: HttpClient){
+    }
 
-  public model = {
- editorData: '<p>Hello, worlddsaf!</p>'
-};
+	public Editor = ClassicEditor;
 
-  public config = {
-ckfinder: {
-  options: {
-    resourceType: 'Images'
-  },
-  uploadUrl: 'http://localhost:3000/test/upload/iamge'
- }
-};
+	public model = {
+		title: '',
+		tags: '',
+		context_type: 'ckeditor',
+		context: ''
+	};
 
-public onChange( { editor } ) {
-const data = editor.getData();
-console.log( data );
-}
+	public config = {
+		ckfinder: {
+			options: {
+				resourceType: 'Images'
+			},
+			uploadUrl: '/drebago/v1/images'
+		},
+		extraPlugins : ['FooPlugin', 'BarPlugin', 'markdown']
+	};
+	// {"fileName":"file name","uploaded":1,"error":{"number":201,"message":""},"url":"image url"}
+
+	public onChange({ editor }) {
+		const data = editor.getData();
+		console.log(data);
+	}
+
+	onSubmit() {
+		console.log('Form submit, model', this.model);
+		this.http.post('/drebago/v1/blog/',this.model).subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+	}
+
+	reset() {
+
+	}
 
 }
